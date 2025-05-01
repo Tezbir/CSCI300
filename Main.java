856f88dfe32f70c883de069ee95a6a7d48bb6f62
Dennis <dennisg0914@gmail.com>
2025-04-30 21:01:52 -0400

    Added a quit case to the main function and closed scanner.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to Some Company! Are you signing in as a customer, an employee, or an admin? (C/E/A)");
+        System.out.println("Welcome to Some Company! Are you signing in as a customer, an employee, or an admin? (C/E/A or type Q to quit)");
         String input = scan.nextLine().toUpperCase();
         switch (input) { // Rather than using if-else statements, we can use a switch statement for better readability.
             case "C":
@@ -14,8 +14,12 @@ public class Main {
             case "A":
                 new AdminSignInPage(); // Change this to how you made the admin login window.
                 break;
+            case "Q":
+                System.out.println("Exiting the program. Goodbye!");
+                break;
             default:
-                System.out.println("Invalid input. Please enter C, E, or A.");
+                System.out.println("Invalid input. Please enter C, E, A or Q.");
         }
+        scan.close(); // Close the scanner to prevent resource leaks.
     }
 }
