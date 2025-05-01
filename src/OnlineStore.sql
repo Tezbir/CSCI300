6203ae3090e5705c91edf95ee517fd1119d69e0b
Tezbir <tezbir1235@yahoo.com>
2025-05-01 13:56:11 -0400

    Update OnlineStore.sql

--- src/OnlineStore.sql
+++ src/OnlineStore.sql
@@ -91,3 +91,85 @@ WHERE order_id = 3;
 
 INSERT INTO online_store.OrderOfItems (order_id, item_id, quantity, item_status)
 VALUES (3, 4, 1, 'completed');
+Create Table online_store.admins(
+admin_id int,
+admin_name varchar(255),
+admin_username varchar(255),
+admin_password varchar(255),
+admin_address varchar(255),
+Primary Key(admin_id));
+insert into
+values(1,'Admin admin','admin','admin','1 Ronkonkoma Avenue, Lake Ronkonkoma, New York');
+insert into
+values(2,'Tezbir Singh','tezbir','Ribzet','10 Spring Yard, Lake Ronkonkoma, New York');
+
+CREATE TABLE online_store.customers (
+    customer_id int,
+    customer_name varchar(255),
+    customer_username varchar(255),
+    customer_password varchar(255),
+    customer_address varchar(255),
+    PRIMARY KEY (customer_id));
+ALTER TABLE customers AUTO_INCREMENT = 1;
+
+ CREATE TABLE online_store.Orders (
+      order_id int,
+ 	 customer_id int,
+      order_date_time DATETIME,
+      delivery_date DATE,
+      coupon_code int,
+      PRIMARY KEY (order_id),  
+ 	 FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+ 	 FOREIGN KEY (coupon_code) REFERENCES Coupons(code_id)
+ );
+
+CREATE TABLE online_store.shipments (
+    shipment_id INT,
+    order_id INT,
+    customer_id INT,
+    PRIMARY KEY (shipment_id),
+    FOREIGN KEY (order_id) REFERENCES admin.orders(order_id),
+    FOREIGN KEY (customer_id) REFERENCES admin.customers(customer_id));
+CREATE TABLE online_store.Employees(
+ 	employee_id int,
+ 	employee_name varchar(255),
+ 	employee_username varchar(255),
+     employee_password varchar(255),
+     employee_tempPassword varchar(255),
+     PRIMARY KEY (employee_id)
+     );
+ CREATE TABLE online_store.Items(
+ 	item_id int,
+     item_name varchar(255),
+     item_price DECIMAL(10,2),
+     quantity_in_stock int,
+     created_by int,
+     PRIMARY KEY (item_id),
+ 	FOREIGN KEY (created_by) REFERENCES Employees(employee_id)
+     );
+ CREATE TABLE online_store.Customers (
+     customer_id int AUTO_INCREMENT,
+     customer_name varchar(255),
+     customer_username varchar(255),
+     customer_password varchar(255),
+     customer_address varchar(255),
+     PRIMARY KEY (customer_id)
+     );
+ 
+ CREATE TABLE online_store.Coupons(
+ 	code_id int,
+     discount_percent DECIMAL(5,2) CHECK (discount_percent >= 0 AND discount_percent <= 100),
+ 	created_by int,
+     PRIMARY KEY (code_id),
+ 	FOREIGN KEY (created_by) REFERENCES Employees(employee_id)
+ );
+
+ CREATE TABLE online_store.OrderOfItems (
+ 	order_id int,
+ 	item_id int,
+     quantity int,
+     item_status varchar(255),
+     PRIMARY KEY (order_id, item_id),
+     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+     FOREIGN KEY (item_id) REFERENCES Items(item_id)
+ );
