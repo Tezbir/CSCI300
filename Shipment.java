cd7e6311aeaf7b3801c446023363b05ec8efc1aa
Tezbir <tezbir1235@yahoo.com>
2025-04-30 23:22:14 -0400

    remove excess info

--- Shipment.java
+++ Shipment.java
@@ -1,9 +1,6 @@
 public class Shipment {
 
     private int shipmentId;
-    private String employeeName;
-    private String employeeUsername;
-    private String employeePassword;
     private int orderId;
     private int customerId;
 
@@ -13,30 +10,6 @@ public class Shipment {
 
     public void setShipmentId(int shipmentId) {
         this.shipmentId = shipmentId;
-    }
-
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-
-    public String getEmployeeUsername() {
-        return employeeUsername;
-    }
-
-    public void setEmployeeUsername(String employeeUsername) {
-        this.employeeUsername = employeeUsername;
-    }
-
-    public String getEmployeePassword() {
-        return employeePassword;
-    }
-
-    public void setEmployeePassword(String employeePassword) {
-        this.employeePassword = employeePassword;
     }
     
     public int getOrderId() {
